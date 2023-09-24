0a84ecee74b7340db2b8e4f53b05e9a166257653
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2023-09-24 14:10:05 +0300

    Finished game. Tha name of the game is "Guess the number"

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,2 +1,23 @@
-name = 'plamen'
-#one more test
+import random
+
+computer_number = random.randint(1, 100)
+
+print(computer_number)
+
+while True:
+    player_input = input("Guess the number (1-100): ")
+
+    if not player_input.isdigit():
+        print(f"Invalide input. Try again...")
+        continue
+
+    player_number = int(player_input)
+    if player_number == computer_number:
+        print(f"You guess it!")
+        break
+
+    elif player_number > computer_number:
+        print(f"Too High!")
+
+    elif player_number < computer_number:
+        print(f"Too Low")
